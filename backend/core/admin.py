cd324953e70ef5e20919e0f3239f1e5233efaee5
AlexCourivaud <alexandre.courivaud@gmail.com>
2025-09-22 01:57:10 +0200

    création admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Project, ProjectMember, Note, Tag, Comment, NoteFavorite
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['name', 'description']
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'project', 'author', 'created_at']
+    list_filter = ['project', 'created_at']
+    search_fields = ['title', 'content']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['note', 'author', 'created_at']
+    list_filter = ['created_at']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+
+
+admin.site.register(ProjectMember)
+admin.site.register(NoteFavorite)
